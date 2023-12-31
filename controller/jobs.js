a1d84a4e0fc25ed809a9d9ca54e2bd3123b5b6b7
Nilesh-Patil0310 <patilnilesh093@gmail.com>
2024-01-01 01:17:44 +0530

    create sign up sing in pages along with there controller and routes for it

---
+++ controller/jobs.js
@@ -0,0 +1,11 @@
+// for external jobs
+const fetch = require("node-fetch");
+const env = require("../config/environment");
+module.exports.jobPage = async function (req, res) {
+  const response = await fetch(env.api_path);
+  const jobsData = await response.json();
+  return res.render("placementCell", {
+    title: "Placement Cell",
+    body: jobsData.jobs,
+  });
+};
