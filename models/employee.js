ea7fa91c8eb1d2a307eec49e4ed34b8433d66640
Nilesh-Patil0310 <patilnilesh093@gmail.com>
2024-01-01 01:15:11 +0530

    create required schemas and configure database and flash meassage

---
+++ models/employee.js
@@ -0,0 +1,26 @@
+// Schema for employee
+const mongoose = require("mongoose");
+const employee = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+const Employee = mongoose.model("Employee", employee);
+module.exports = Employee;
---
+++ models/interview.js
@@ -0,0 +1,29 @@
+// Schema for interviewss
+const mongoose = require("mongoose");
+const interview = new mongoose.Schema(
+  {
+    companyName: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now(),
+    },
+    students: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Student",
+      },
+    ],
+    result: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Result",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+const Interview = mongoose.model("Interview", interview);
+module.exports = Interview;
